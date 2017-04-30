4a3241eecadb37e5c0cdb3bf1270422413c88a07
노우현 <rkjnice@gmail.com>
2017-04-30 15:25:14 +0900

    indent fix

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -864,6 +864,7 @@ class OGame(object):
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         return True
 
+
     def send_minifleet_spy(self, where, ship_count, token):
         headers = {'X-Requested-With': 'XMLHttpRequest'}
         payload = {'mission': 6,
@@ -983,19 +984,20 @@ class OGame(object):
         delete_action = self.session.post(self.get_url('planetGiveup'), headers=headers, data=delete_payload).content   
 
 
-def Consommation(type, batiment, lvl):
-
-    """ Retourne la consommation du batiment du level lvl + 1 """
-    energieLvl = constants.Formules[type][batiment]['consommation'][0] * lvl * (constants.Formules[type][batiment]['consommation'][1]**lvl)
-    energieNextLvl = constants.Formules[type][batiment]['consommation'][0] * (lvl+1) * (constants.Formules[type][batiment]['consommation'][1]**(lvl+1))
-    return math.floor(energieNextLvl - energieLvl)
 
 
-def building_cost(type, batiment, lvl):
-    """ Retourne le cout d'un batiment lvl + 1 """
-    cost = {}
-    cost['metal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Metal'][0]*constants.Formules[type][batiment]['cout']['Metal'][1]**(lvl-1)))
-    cost['crystal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Crystal'][0]*constants.Formules[type][batiment]['cout']['Crystal'][1]**(lvl-1)))
-    cost['deuterium'] = int(math.floor(constants.Formules[type][batiment]['cout']['Deuterium'][0]*constants.Formules[type][batiment]['cout']['Deuterium'][1]**(lvl-1)))
-    return cost
+    def Consommation(type, batiment, lvl):
+        """ Retourne la consommation du batiment du level lvl + 1 """
+        energieLvl = constants.Formules[type][batiment]['consommation'][0] * lvl * (constants.Formules[type][batiment]['consommation'][1]**lvl)
+        energieNextLvl = constants.Formules[type][batiment]['consommation'][0] * (lvl+1) * (constants.Formules[type][batiment]['consommation'][1]**(lvl+1))
+        return math.floor(energieNextLvl - energieLvl)
+
+    def building_cost(type, batiment, lvl):
+        """ Retourne le cout d'un batiment lvl + 1 """
+        cost = {}
+        cost['metal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Metal'][0]*constants.Formules[type][batiment]['cout']['Metal'][1]**(lvl-1)))
+        cost['crystal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Crystal'][0]*constants.Formules[type][batiment]['cout']['Crystal'][1]**(lvl-1)))
+        cost['deuterium'] = int(math.floor(constants.Formules[type][batiment]['cout']['Deuterium'][0]*constants.Formules[type][batiment]['cout']['Deuterium'][1]**(lvl-1)))
+        return cost
+
 
