8aeaf9be3fc6e3966998713df3c2b1681a5cdd26
Giancarlo Ventura <gventurag@unprg.edu.pe>
2017-06-02 19:52:54 -0500

    Update constants.py

--- ogame/constants.py
+++ ogame/constants.py
@@ -155,4 +155,5 @@ Missions = {'Attack': 1,
             'RecycleDebrisField': 8,
             'Destroy': 9,
             'Expedition': 15,
-            'DeployToMoon': 4}
+            'DeployToMoon': 4,
+            'DeployToPlanet': 4}
