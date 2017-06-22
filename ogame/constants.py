b45f3784fbdc8e136db0e77f15814d68b7cdeb2e
Giancarlo Ventura <ForPequi@users.noreply.github.com>
2017-06-22 17:08:12 -0500

    Update constants.py

--- ogame/constants.py
+++ ogame/constants.py
@@ -18,7 +18,18 @@ Buildings = {'MetalMine': 1,
              'Fusiecentrale': 12,
              'Metaalopslag': 22,
              'Kristalopslag': 23,
-             'Deuteriumtank': 24}
+             'Deuteriumtank': 24
+
+             'metal_mine': 1,
+             'deuterium_synthesizer': 3,
+             'solar_plant': 4,
+             'solar_satellite': 212,
+             'deuterium_tank': 24,
+             'crystal_mine': 2,
+             'fusion_reactor': 12,
+             'metal_storage': 22,
+             'crystal_storage': 23
+             }
 
 
 Facilities = {'AllianceDepot': 34,
